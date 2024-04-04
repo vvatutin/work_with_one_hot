8463cdf3671bb14d2bd564a6181676aa340fdc47
vvatutin <161581923+vvatutin@users.noreply.github.com>
2024-04-04 10:34:54 +0500

    Create hw_44.py

---
+++ hw_44.py
@@ -0,0 +1,30 @@
+'''
+Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего
+из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без
+get_dummies?
+'''
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+data.head()
+
+print(data)
+
+# Используем get_dummies
+
+print(pd.get_dummies(data, columns=['whoAmI']))
+
+# Не используем get_dummies
+
+data = pd.DataFrame({'whoAmI':lst})
+
+data.loc[data['whoAmI'] == 'human', 'human'] = '1'
+data.loc[data['whoAmI'] != 'human', 'human'] = '0'
+data.loc[data['whoAmI'] == 'robot', 'robot'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robot'] = '0'
+
+print(data)
